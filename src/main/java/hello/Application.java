f2c6e21a0792ca6995ce3cdda4388bacfbeb5938
shibatako <koki305@gmail.com>
2014-04-28 02:49:33 +0900

    db accsess

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,12 +1,14 @@
 package hello;
 
 import javax.servlet.MultipartConfigElement;
+import javax.sql.DataSource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.MultiPartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @ComponentScan
 @EnableAutoConfiguration
@@ -18,6 +20,16 @@ public class Application {
         factory.setMaxFileSize("128KB");
         factory.setMaxRequestSize("128KB");
         return factory.createMultipartConfig();
+    }
+	
+	@Bean
+    protected DataSource createDataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(org.h2.Driver.class);
+        dataSource.setUsername("sa");
+        dataSource.setUrl("jdbc:h2:tcp://127.0.1.1/~/test");
+        dataSource.setPassword(""); 
+        return dataSource;
     }
 
     public static void main(String[] args) {
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,13 +2,22 @@ package hello;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,12 +34,6 @@ public class GreetingController {
         return "greeting";
     }
     
-    @RequestMapping(value = "/greetings")
-    public String greetings(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", "hoge");
-        return "greeting";
-    }
-    
     @RequestMapping(value = "/greeting_unsafe")
 	public String checkXSS(Model model){
 		model.addAttribute("name", "<strong>strongTag</strong>");
@@ -42,9 +45,6 @@ public class GreetingController {
 		Resource file = new FileSystemResource("src/main/webapp/tmp/sample.txt");
 		System.out.println(file.toString());
 		response.setContentType("text/plain");
-		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-		System.out.println((int)FileUtils.sizeOf(file.getFile()));
-		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
 		response.setContentLength((int)FileUtils.sizeOf(file.getFile()));
 		response.setHeader("Content-Disposition","attachment; filename=\"" + file.getFile().getName() +"\"");
 		FileCopyUtils.copy(file.getInputStream(), response.getOutputStream());
@@ -63,5 +63,46 @@ public class GreetingController {
 		model.addAttribute("filepath", "tmp/" + myFile.getOriginalFilename());
 		return "attachment";
 	} 
+    
+    
+    @Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private PlatformTransactionManager platformTransactionManager;
+	
+	@Transactional
+	@RequestMapping("/moke")
+	public String moke4ing(Model model) throws ClassNotFoundException, SQLException {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		//データの登録
+		jdbcTemplate.execute("drop table customers if exists");
+		jdbcTemplate.execute("create table customers(" +
+				"id serial, first_name varchar(255), last_name varchar(255))");
+
+		String[] names = "John Woo;Jeff Dean;Josh Bloch;Josh Long".split(";");
+		for (String fullname : names) {
+			String[] name = fullname.split(" ");
+			//System.out.printf("Inserting customer record for %s %s\n", name[0], name[1]);
+			jdbcTemplate.execute(
+					"INSERT INTO customers(first_name,last_name) values('" + name[0] + "','" + name[1] + "')");
+		}
+
+		//登録したデータの検索
+		List<Customer> list = jdbcTemplate.query("select * from customers", 
+				new RowMapper<Customer>() {
+			@Override
+			public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new Customer(rs.getLong("id"), rs.getString("first_name"),
+						rs.getString("last_name"));
+			}
+		});
+
+		model.addAttribute("customers", list);
+		model.addAttribute("name", list.get(0).toString());
+
+		return "thymeleaf";
+
+	}
 
 }
